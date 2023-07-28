a4b61a89496d1dc00aad3f3ecaba6586dae4f981
alhumaw <alhumaw47@gmail.com>
2023-07-28 13:50:10 -0700

    getIpAddress

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
 // script.js
 import $ from 'jquery';
 
-
-function updateVisitCount() {
-    $.ajax({
-        url: 'https://y36d9igl7f.execute-api.us-west-1.amazonaws.com/Peer_Live/peer?ip=0.0.0.0',
-        type: 'GET',
-        dataType: 'json',
-        success: function (data) {
-            // Update the visit count on the web page
-            $('#visitCount').text(data.visitCount);
-        },
-        error: function (error) {
-            console.error('Failed to update visit count:', error);
-        }
-        
-    });
+function getIPAddress() {
+    fetch('https://api.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => {
+            // Update the paragraph element with the IP address
+            document.getElementById('ip-address').textContent = data.ip;
+        })
+        .catch(error => {
+            console.error('Error fetching IP address:', error);
+        });
 }
 
 // Call the function to update the visit count when the page loads
 $(document).ready(function () {
-    updateVisitCount();
+    getIPAddress();
 });
-
-
