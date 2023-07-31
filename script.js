15b0c9c5481c58d9638d7ac287bae404cc22305f
alhumaw <alhumaw47@gmail.com>
2023-07-31 16:30:10 -0700

    updateCount, getCounter, stuff

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+
+function getCounter() {
+    fetch('https://5asnqpgke4.execute-api.us-east-1.amazonaws.com/Live/')
+        .then(response => response.json())
+        .then(data => {
+            // Update the paragraph element with the IP address
+            let curCount = data.count;
+            console.log('Count:', curCount);
+            // Call the function to display the IP address and post it
+            updateCount(curCount);
+        })
+        .catch(error => {
+            console.error('Error fetching count:', error);
+        });
+}
+
+function updateCount(count){
+    const currentCount = "Total Unique Visitors: " + count;
+    document.getElementById('count').textContent = currentCount;
+    
+}
+
+
 function getIPAddress() {
     fetch('https://api.ipify.org?format=json')
         .then(response => response.json())
@@ -28,11 +51,14 @@ function updateIPAndPost(ipAddress) {
     const formattedIP = "Your IP: " + ipAddress;
     document.getElementById('ip-address').textContent = formattedIP;
     postIPAddress(ipAddress);
+    
 }
+
 
 // Call the function to update the visit count when the page loads
 $(document).ready(function () {
     getIPAddress();
+    getCounter();
 });
 
 console.log("JS DONE RUNNING");
